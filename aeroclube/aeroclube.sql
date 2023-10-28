f334377aaad2cc419c76e490edaef744fd0db46f
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2023-10-28 19:18:55 -0300

    add caso de uso Aeroclube

---
+++ aeroclube/aeroclube.sql
@@ -0,0 +1,17 @@
+/*******************************************************************************
+	Esquema de Relações - Aeroclube
+********************************************************************************
+Cliente (Cod_Cliente Integer PK NN, Nome VARCHAR, Sexo CHAR)
+
+Hospede (Cod_Hospede Integer PK NN, Nome VARCHAR, Data_Nasc DATE NN, Cod_Client FK,
+Parentesco_Cliente VARCHAR NN) 
+Cod_Cliente Referencia Cliente
+
+
+
+
+
+
+
+
+*******************************************************************************/
